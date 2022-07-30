26cadc22b4600178b3ae5e521933cabeebe2951a
Hasan Javed <rana.hasanjaved@gmail.com>
2022-07-30 13:54:30 +0500

    vectorsubsetting

--- house.pets.R
+++ house.pets.R
@@ -3,5 +3,10 @@ rate <-c(5,2,30)
 feed <-c("yes","","No")
 run <-c(1,NA,10)
 house.pets <-data.frame(Type=pets,rate,feed, run)
-
-
+x <- c("a","b","c","d","c","a")
+x[1]
+x[2]
+x[1:5]
+x[x > "a"]
+u <- x>"a"
+u
